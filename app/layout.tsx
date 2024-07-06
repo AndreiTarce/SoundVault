7e30fb8230795e58d5a48a9584e8e4b1a5d4de62
AndreiTarce <tarceandrei@gmail.com>
2024-07-06 16:45:31 +0300

    set up project folder structure, mongodb, tailwind, shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import { Inter as FontSans } from "next/font/google";
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={fontSans.className}>{children}</body>
+        </html>
+    );
 }
